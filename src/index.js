5f7942966841e538bcfa273e359a3441f03c0e9c
Hidayat Abisena <plegmasena@gmail.com>
2022-11-25 14:06:55 +0700

    Implementing UPDATE / PATCH API request

--- src/index.js
+++ src/index.js
@@ -29,6 +29,17 @@ app.post("/notes", async (req, res) => {
   }
 });
 
+app.patch("/notes/:id", async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    note.note = req.body.note;
+    await note.save();
+    res.status(200).send(note);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
 app.listen(3000, () => {
   console.log("server is up on port 3000");
 });
